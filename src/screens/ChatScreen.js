c4b2842fa72b3b367eea65b417ded3219dbc6879
moFahmi10 <Boodabee27>
2023-04-18 05:17:02 +0200

    Added user info in chat header

--- src/screens/ChatScreen.js
+++ src/screens/ChatScreen.js
@@ -1,10 +1,15 @@
-import React, {useState, useCallback, useEffect, useContext} from 'react';
-import {GiftedChat} from 'react-native-gifted-chat';
+import React, {useState, useCallback, useEffect, useContext, useLayoutEffect} from 'react';
+import {Bubble, Composer, GiftedChat, InputToolbar, Send} from 'react-native-gifted-chat';
 import {CurrentUserContext} from '../providers/CurrentUserProvider';
 import ChatServices from '../services/ChatServices';
 import Chat from '../models/Chat';
 import ScreenLoadingIndicator from '../widgets/ScreenLoadingIndicator';
-import {useRoute} from '@react-navigation/native';
+import {useNavigation, useRoute} from '@react-navigation/native';
+import MenuImage from "../widgets/MenuImage";
+import {View, Text, Image, Button, TouchableOpacity} from "react-native";
+import {appPurpleDark} from "../utilities/constants";
+import ImagePickerButton from "../widgets/ImagePickerButton";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
 
 
 export function ChatScreen() {
@@ -12,8 +17,45 @@ export function ChatScreen() {
     const {currentUser} = useContext(CurrentUserContext);
     const route = useRoute();
     const {chat} = route.params;
-    const [isFirst,setIsFirst] = useState(true);
+    const navigation = useNavigation();
+   // const [name,setName] = useState("")
+   // const [imageUri,setImageUri] = useState("")
+    let name = ""
+    let imageUri = ""
+    if (currentUser === chat.userThatPostedId ){
+        // setName(chat.userThatRequestedFullName)
+        // setImageUri(chat.userThatRequestedProfilePicture)
+        name = chat.userThatRequestedFullName
+        imageUri = chat.userThatRequestedProfilePicture
+    }else {
+        // setName(chat.userThatPostedFullName)
+        // setImageUri(chat.userThatPostedProfilePicture)
+        name = chat.userThatPostedFullName
+        imageUri = chat.userThatPostedProfilePicture
+    }
+    useLayoutEffect(() => {
 
+
+        navigation.setOptions({
+
+            headerTitle: "",
+            headerRight: () => <View>
+                <View style={{flexDirection: 'row',marginRight:"55%",marginTop:"3%"}}>
+                <Image source={{uri: imageUri}} style={{
+                    marginRight: "5%",
+                    borderRadius: 50,
+                    height: 40,
+                    width: 40,
+                }}></Image>
+                <Text style={{fontSize:17, marginTop:"5%",color:"white",fontWeight:"bold"}}>{name}</Text>
+                </View>
+                <View style={{flexDirection: 'row',marginRight:"35%"}}>
+                    <Text style={{fontSize:15, marginTop:"3%",color:"white",fontWeight:"600",marginLeft:"1%",marginBottom:"2%"}}>{`${chat.petName} ${chat.postType} `}</Text>
+                </View>
+
+            </View>,
+        });
+    }, []);
 
     useEffect(() => {
 
@@ -59,8 +101,8 @@ export function ChatScreen() {
             createdAt: message.createdAt.toDate(),
             user: {
                 _id: message.uid,
-                name: currentUser.uid === chat.userThatPostedId ? chat.userThatPostedFullName : chat.userThatRequestedFullName,
-                avatar: currentUser.uid === chat.userThatPostedId ? chat.userThatPostedProfilePicture : chat.userThatRequestedProfilePicture,
+                name: currentUser.uid === chat.userThatPostedId ? chat.userThatRequestedFullName: chat.userThatPostedFullName ,
+                avatar: currentUser.uid === chat.userThatPostedId ? chat.userThatRequestedProfilePicture: chat.userThatPostedProfilePicture  ,
             },
         };
     };
@@ -72,6 +114,28 @@ export function ChatScreen() {
             message.user.name
         ).then(()=>console.log("Message sent"));
     }, []);
+    const renderBubble = (props) => {
+        return (
+            <Bubble
+                {...props}
+                wrapperStyle={{
+                    right: {
+                        backgroundColor: appPurpleDark
+                    }
+                }}
+            />
+        );
+    };
+    const renderSend = (props) => {
+        return (
+            <Send {...props} disabled={!props.text}>
+
+                    <FontAwesome name={"paper-plane"} style={{fontSize:27,marginRight:"5%",marginBottom:"15%",color:appPurpleDark}}></FontAwesome>
+
+            </Send>
+        );
+    };
+
 
         return (
             <GiftedChat
@@ -82,6 +146,13 @@ export function ChatScreen() {
                     name: currentUser.fullName,
                     avatar: currentUser.profilePicture,
                 }}
+                renderBubble={renderBubble}
+
+                renderInputToolbar={(props) => (
+                    <InputToolbar {...props} renderSend={renderSend}   />
+                )}
+
+
             />
         );
 
